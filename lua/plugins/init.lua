05f99cc44c90956470c4b2adcfbed51af9350b2c
Yacine <yacinebenalidayat@gmail.com>
2024-05-04 15:35:26 +0100

    my config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,31 +8,43 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+        "html-lsp", "css-lsp",
+        "typescript-language-server",  "eslint_d",
+        "prettier",
+        "rust-analyzer",
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+        -- defaults 
+  			"vim", "lua", "vimdoc",
+
+        -- web dev
+        "html", "css", "javascript", "typescript",
+
+        -- web server dev
+        "go",
+
+       -- low level
+        "rust",
+  		},
+  	},
+  },
 }
